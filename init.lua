67eba9b5aea390e415b416460547e9b3a5f51972
jrstaple <joshrstapleton@gmail.com>
2025-05-19 13:01:47 -0400

    move mason setup to configs directory

--- init.lua
+++ init.lua
@@ -2,9 +2,6 @@
 require("config.lazy")
 require("mason").setup()
 require("mason-lspconfig").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = { "gopls", "pylsp", "lua_ls", "rust_analyzer" },
-})
 
 vim.opt.tabstop = 4
 vim.opt.expandtab = true -- insert spaces instead of \t
---
+++ lua/plugins/configs/mason.lua
@@ -0,0 +1,15 @@
+return {
+  ensure_installed = {
+    "gopls",
+    "gofumpt",
+    "goimports-reviser",
+    "golines",
+    "pylsp",
+    "lua-lanugage-server",
+    "stylua",
+    "rust-analyzer",
+    "rustfmt",
+    "html-lsp",
+    "clangd",
+  },
+}
