161998e0a1b2b6d0f5d2eb810dbaad310f775e30
jrstaple <joshrstapleton@gmail.com>
2025-10-22 16:51:39 -0400

    update plugins to use ruff instead of pylsp

--- init.lua
+++ init.lua
@@ -3,6 +3,16 @@ require("config.lazy")
 require("mason").setup()
 require("mason-lspconfig").setup()
 
+vim.lsp.config("ruff", {
+  init_options = {
+    settings = {
+      lineLength = 100,
+    },
+  },
+})
+
+vim.lsp.enable("ruff")
+
 vim.opt.tabstop = 4
 vim.opt.expandtab = true -- insert spaces instead of \t
 vim.opt.softtabstop = 4
